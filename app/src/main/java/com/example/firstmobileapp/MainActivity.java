ed7638d3cc197335a05e47a236eb86bbd070e5b2
Denis <denis.seremet@snt.md>
2023-03-12 15:26:58 +0200

    Fragments

--- app/src/main/java/com/example/firstmobileapp/MainActivity.java
+++ app/src/main/java/com/example/firstmobileapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.firstmobileapp;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
@@ -13,16 +15,47 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn_fragment1, btn_fragment2;
+    FirstFragment firstFragment = new FirstFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        btn_fragment1 = findViewById(R.id.btn_fragment1);
+        btn_fragment2 = findViewById(R.id.btn_fragment2);
 
+        setNewFragment(firstFragment);
+        btn_fragment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                setNewFragment(firstFragment);
+            }
+
+
+        });
+        btn_fragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SecondFragment secondFragment = new SecondFragment();
+                setNewFragment(secondFragment);
+            }
+
+
+        });
+
+    }
+    private void setNewFragment(Fragment fragment) {
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.framelayout, fragment);
+        ft.commit();
+    }
 }
